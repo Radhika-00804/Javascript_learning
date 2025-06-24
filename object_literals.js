14379979fabd68157d0191b0170ef251b47e073d
Radhika-00804 <163717432+Radhika-00804@users.noreply.github.com>
2025-06-24 14:35:22 +0000

    continuation of object

--- object_literals.js
+++ object_literals.js
@@ -14,11 +14,30 @@ const JsUser = {
 // JsUser.email = "pihu2312@gmail.com"
 // // Object.freeze(JsUser)
 
-// console.log(JsUser.email)
-// console.log(JsUser)
+//  
 
-JsUser.greeting = function(){
-    console.log("Hello JS User")
-}
+// console.log(JsUser.greeting)
+///////////////////////////////////////////////////
 
-console.log(JsUser.greeting)
+// const gptUser = new Object()
+const gptUser = {}
+
+gptUser.id = "radhe"
+gptUser.name = "Radhika"
+
+// console.log(gptUser)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign(obj1, obj2) // if we use this it is in the form target,source so we use this below format
+// const obj3 = Object.assign({},obj1, obj2)
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+// console.log(obj1)
+console.log(Object.keys(gptUser))
+console.log(Object.values(gptUser))
+console.log(Object.entries(gptUser))
+
+console.log(gptUser.hasOwnProperty('id'))
