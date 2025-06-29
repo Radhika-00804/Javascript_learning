273867a91dd78c5d8852e55414e6a93d55dd6451
Radhika-00804 <163717432+Radhika-00804@users.noreply.github.com>
2025-06-29 14:47:06 +0000

    functions

---
+++ function_object.js
@@ -0,0 +1,17 @@
+// function myname(){
+//     console.log("Radhika")
+// }
+
+// myname()
+
+// function addition(num1, num2){
+//     console.log(num1+num2)
+// }
+
+// addition(2,3)
+
+function login_msg(username){
+    return `${username} just logged in`
+}
+
+console.log(login_msg("radhika"))
